cf440171452c4809975f8b37b55c6995dd17f615
Eduardo PL <mini0n.000@gmail.com>
2023-03-15 01:28:07 -0600

    Refactor + Improved tests

--- lib/card.rb
+++ lib/card.rb
@@ -7,20 +7,24 @@ require "byebug"
 # Manages the Card logic including:
 # Numbers, limit, balance, validation
 class Card
+  DEFAULT_BALANCE = 0
+
   attr_accessor :number, :limit, :balance, :valid
 
   def initialize(number: nil, limit: 0)
     @number = number
-    @limit = limit
-    @balance = 0
+    @limit = new_limit(limit)
     @valid = valid?
+    @balance = DEFAULT_BALANCE
   end
 
+  # Checks if the Card's number is valid
   def valid?
     return false if number.blank?
     CreditCardValidations::Luhn.valid?(number)
   end
 
+  # Addes balance to the Card
   def charge(added_balance)
     if added_balance.is_a?(Integer) && added_balance.positive? && valid?
       new_balance = self.balance + added_balance
@@ -31,6 +35,7 @@ class Card
     balance
   end
 
+  # Substracts balanace from the Card
   def credit(substracted_balance)
     if substracted_balance.is_a?(Integer) && substracted_balance.positive? && valid?
       self.balance -= substracted_balance
@@ -39,4 +44,10 @@ class Card
     balance
   end
 
+  private
+
+  def new_limit(new_limit)
+    return new_limit if new_limit.is_a?(Integer) && new_limit.positive?
+    0
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,36 +4,91 @@ require "card.rb"
 require "byebug"
 
 RSpec.describe Card do
+  describe "constants" do
+    it "has DEFAULT_BALANCE constant" do
+      expect(described_class::DEFAULT_BALANCE).to eq(0)
+    end
+  end
 
   describe "#new" do
     it "creates a Card with a valid number & limit" do
       subject = described_class.new(number: "4111111111111111", limit: 1000)
 
-      expect(subject.is_a?(Card)).to be true
+      expect(subject.is_a?(described_class)).to be true
       expect(subject).to have_attributes(number: "4111111111111111",
-                                        limit: 1000, balance: 0, valid: true)
+                                         limit: 1000, balance: 0, valid: true)
     end
 
-    it "creates a Card with a invalid number & limit" do
+    it "creates a Card with an invalid number & limit" do
       subject = described_class.new(number: "4111111111111112", limit: 1000)
 
-      expect(subject.is_a?(Card)).to be true
+      expect(subject.is_a?(described_class)).to be true
       expect(subject).to have_attributes(number: "4111111111111112",
-                                        limit: 1000, balance: 0, valid: false)
+                                         limit: 1000, balance: 0, valid: false)
+    end
+
+    it "creates a Card with a valid number & a non-integer limit" do
+      subject = described_class.new(number: "4111111111111111", limit: 1000.43)
+
+      expect(subject.is_a?(described_class)).to be true
+      expect(subject).to have_attributes(number: "4111111111111111",
+                                         limit: 0, balance: 0, valid: true)
+    end
+
+    it "creates a Card with a valid number & a negative limit" do
+      subject = described_class.new(number: "4111111111111111", limit: -1000)
+
+      expect(subject.is_a?(described_class)).to be true
+      expect(subject).to have_attributes(number: "4111111111111111",
+                                         limit: 0, balance: 0, valid: true)
     end
 
     it "creates a Card without a number or limit" do
-      subject = described_class.new()
+      subject = described_class.new
 
-      expect(subject.is_a?(Card)).to be true
+      expect(subject.is_a?(described_class)).to be true
       expect(subject).to have_attributes(number: nil, limit: 0, balance: 0, valid: false)
+    end
+  end
+
+  describe "new_limit" do
+    context "when a valid Card limit is passed, it is returned" do
+      it "returns the new limit" do
+        subject = described_class.new
+
+        expect(subject.new_limit(4300)).to eq 4300
+      end
+    end
+
+    context "when an empty (nil) Card limit is passed" do
+      it "returns zero" do
+        subject = described_class.new
+
+        expect(subject.new_limit(nil)).to eq 0
+      end
+    end
+
+    context "when a non-Integer Card limit is passed" do
+      it "returns zero" do
+        subject = described_class.new
+
+        expect(subject.new_limit(4300.43)).to eq 0
+      end
+    end
+
+    context "when a negative Card limit is passed" do
+      it "returns zero" do
+        subject = described_class.new
+
+        expect(subject.new_limit(-4300)).to eq 0
+      end
     end
   end
 
   describe "#valid?" do
     context "when a valid card number is used" do
       it "returns true" do
-        subject = described_class.new()
+        subject = described_class.new
 
         subject.number = "585227606647" # 12 digits
         expect(subject.valid?).to be true
@@ -63,7 +118,7 @@ RSpec.describe Card do
 
     context "when an invalid car number is used" do
       it "returns false" do
-        subject = described_class.new()
+        subject = described_class.new
 
         subject.number = nil # nil
         expect(subject.valid?).to be false
@@ -99,7 +154,7 @@ RSpec.describe Card do
   end
 
   describe "#charge" do
-    context "increases the balance of a Card object" do
+    context "when validly called it increases the balance of a Card object" do
       it "returns the Card's new balance" do
         subject = described_class.new(number: "4111111111111111", limit: 1000)
 
@@ -151,7 +206,7 @@ RSpec.describe Card do
   end
 
   describe "#credit" do
-    context "substracts balance from a Card object" do
+    context "when validly called it substracts balance from a Card object" do
       it "returns the Card's new balance" do
         subject = described_class.new(number: "4111111111111111", limit: 1000)
         subject.charge(430)
@@ -206,6 +261,4 @@ RSpec.describe Card do
       end
     end
   end
-
-
 end
