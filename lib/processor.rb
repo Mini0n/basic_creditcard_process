2f7ebb7bcebd74c57d85ad9e1ffd982dc7fb0fc7
Eduardo PL <mini0n.000@gmail.com>
2023-03-18 03:50:39 -0600

    Processor & specs

--- lib/processor.rb
+++ lib/processor.rb
@@ -6,32 +6,96 @@ require "byebug"
 # Procesor Class
 # Handles instructions decoding & execution
 class Processor
-  PARAM_SEPARATOR = " "
-  OPERATIONS = %w[add charge credit].freeze
+  attr_accessor :user_list
 
   NAME_REGEX = / [a-zA-Z]+/.freeze # only letters (lower & upper)
   AMOUNT_REGEX = / \$[0-9]+/.freeze # only numbers starting with '$'
   CARD_NUMBER_REGEX = / [0-9]{1,19}/.freeze # only digits (from 1 up to 19)
-  OPERATIONS_REGEX = /#{OPERATIONS.join("|")}/i.freeze # only defined operations (lower & uper)
 
 
-  # Parse an instruction line
+  def initialize
+    @user_list = UserList.new
+  end
+
+  # Parse an instruction an line and
+  # return a hash with the instruction type & parameters
   def parse_instruction(instruction: nil)
     return unless valid_instruction?(instruction)
 
+    instruction_data = instruction.split(" ")
+
     case instruction_type(instruction)
     when :add
-      return "add"
+      {
+        instruction: :add,
+        user_name: instruction_data[1],
+        card_number: instruction_data[2],
+        card_limit: instruction_data[3]
+      }
     when :charge
-      return "charge"
+      {
+        instruction: :charge,
+        user_name: instruction_data[1],
+        charge_amount: instruction_data[2]
+      }
     when :credit
-      return "credit"
+      {
+        instruction: :credit,
+        user_name: instruction_data[1],
+        credit_amount: instruction_data[2]
+      }
     end
   end
 
+  # Call the required method to execute the instruction hash
+  def process_instruction_hash(instruction_hash: nil)
+    return unless instruction_hash.is_a?(Hash) && instruction_hash.present?
 
+    case instruction_hash&.[](:instruction)
+    when :add
+      process_add(instruction_hash)
+    when :charge
+      process_charge(instruction_hash)
+    when :credit
+      process_credit(instruction_hash)
+    end
+  end
+
+  # Parses an String instruction and then processes it
+  def process_instruction(instruction: nil)
+    instruction_hash = parse_instruction(instruction: instruction)
+    return unless instruction_hash
+
+    process_instruction_hash(instruction_hash: instruction_hash)
+  end
 
   private
+
+  def process_add(instruction_data)
+    limit = parse_amount(instruction_data[:card_limit])
+    add_user_card = Card.new(number: instruction_data[:card_number], limit: limit)
+    add_user = User.new(name: instruction_data[:user_name], card: add_user_card)
+
+    user_list.add_user(user: add_user)
+  end
+
+  def process_charge(instruction_data)
+    charge_amount = parse_amount(instruction_data[:charge_amount])
+    charged_user = user_list.find_user_by_name(name: instruction_data[:user_name])
+    charged_user.charge(charge_amount)
+  end
+
+  def process_credit(instruction_data)
+    credit_amount = parse_amount(instruction_data[:credit_amount])
+    credited_user = user_list.find_user_by_name(name: instruction_data[:user_name])
+    credited_user.credit(credit_amount)
+  end
+
+  def parse_amount(amount)
+    return 0 unless amount.is_a?(String) && (amount =~ /^\$[0-9]+$/) == 0
+
+    amount[1..].to_s.to_i
+  end
 
   def instruction_type(instruction)
     return :add if (instruction =~ add_regex) == 0
@@ -60,9 +124,4 @@ class Processor
   def credit_regex
     @credit_regex ||= Regexp.new("#{/credit/i}#{NAME_REGEX}#{AMOUNT_REGEX}")
   end
-
-
-
-
-
 end
--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -4,27 +4,19 @@ require "processor"
 require "byebug"
 
 RSpec.describe Processor do
+  subject(:processor) { described_class.new }
+
   describe "constants" do
-    it "has PARAM_SEPARATOR" do
-      expect(described_class::PARAM_SEPARATOR).not_to be_nil
-    end
-
-    it "has OPERATIONS" do
-      expect(described_class::OPERATIONS).to be_a(Array)
-    end
-
     it "has Regex constants" do
       expect(described_class::NAME_REGEX).to be_a(Regexp)
       expect(described_class::AMOUNT_REGEX).to be_a(Regexp)
       expect(described_class::CARD_NUMBER_REGEX).to be_a(Regexp)
-      expect(described_class::OPERATIONS_REGEX).to be_a(Regexp)
     end
   end
 
   describe "#add_regex" do
     it "returns add instruction parsing Regexp" do
-      subject = described_class.new
-      result_regex = subject.send(:add_regex)
+      result_regex = processor.send(:add_regex)
 
       expect(result_regex).to be_a(Regexp)
       expect(result_regex.to_s).to include(/add/i.to_s)
@@ -36,8 +28,7 @@ RSpec.describe Processor do
 
   describe "#charge_regex" do
     it "returns charge instruction parsing Regexp" do
-      subject = described_class.new
-      result_regex = subject.send(:charge_regex)
+      result_regex = processor.send(:charge_regex)
 
       expect(result_regex).to be_a(Regexp)
       expect(result_regex.to_s).to include(/charge/i.to_s)
@@ -48,8 +39,7 @@ RSpec.describe Processor do
 
   describe "#credit_regex" do
     it "returns credit instruction parsing Regexp" do
-      subject = described_class.new
-      result_regex = subject.send(:credit_regex)
+      result_regex = processor.send(:credit_regex)
 
       expect(result_regex).to be_a(Regexp)
       expect(result_regex.to_s).to include(/credit/i.to_s)
@@ -60,46 +50,41 @@ RSpec.describe Processor do
 
   describe "#instruction_regex" do
     it "returns an instruction parsing Regexp" do
-      subject = described_class.new
-      result_regex = subject.send(:instruction_regex)
+      result_regex = processor.send(:instruction_regex)
 
       expect(result_regex).to be_a(Regexp)
-      expect(result_regex.to_s).to include(subject.send(:add_regex).to_s)
-      expect(result_regex.to_s).to include(subject.send(:charge_regex).to_s)
-      expect(result_regex.to_s).to include(subject.send(:credit_regex).to_s)
+      expect(result_regex.to_s).to include(processor.send(:add_regex).to_s)
+      expect(result_regex.to_s).to include(processor.send(:charge_regex).to_s)
+      expect(result_regex.to_s).to include(processor.send(:credit_regex).to_s)
     end
   end
 
   describe "#valid_instrucion?" do
     context "when a valid instruction is passed" do
       it "returns true" do
-        subject = described_class.new
-
-        expect(subject.send(:valid_instruction?, "Add Tom 4111111111111111 $1000")).to be true
-        expect(subject.send(:valid_instruction?, "Add Lisa 5454545454545454 $3000")).to be true
-        expect(subject.send(:valid_instruction?, "Add Quincy 1234567890123456 $2000")).to be true
-        expect(subject.send(:valid_instruction?, "Charge Tom $500")).to be true
-        expect(subject.send(:valid_instruction?, "Charge Tom $800")).to be true
-        expect(subject.send(:valid_instruction?, "Charge Lisa $7")).to be true
-        expect(subject.send(:valid_instruction?, "Credit Lisa $100")).to be true
-        expect(subject.send(:valid_instruction?, "Credit Quincy $200")).to be true
+        expect(processor.send(:valid_instruction?, "Add Tom 4111111111111111 $1000")).to be true
+        expect(processor.send(:valid_instruction?, "Add Lisa 5454545454545454 $3000")).to be true
+        expect(processor.send(:valid_instruction?, "Add Quincy 1234567890123456 $2000")).to be true
+        expect(processor.send(:valid_instruction?, "Charge Tom $500")).to be true
+        expect(processor.send(:valid_instruction?, "Charge Tom $800")).to be true
+        expect(processor.send(:valid_instruction?, "Charge Lisa $7")).to be true
+        expect(processor.send(:valid_instruction?, "Credit Lisa $100")).to be true
+        expect(processor.send(:valid_instruction?, "Credit Quincy $200")).to be true
       end
     end
 
-    context "when an ivalid instruction is passed" do
+    context "when an invalid instruction is passed" do
       it "returns false" do
-        subject = described_class.new
-
-        expect(subject.send(:valid_instruction?, "")).to be false
-        expect(subject.send(:valid_instruction?, "Add Tom $1000")).to be false
-        expect(subject.send(:valid_instruction?, "Add Tom 41111111111111111")).to be false
-        expect(subject.send(:valid_instruction?, "Add Tom 411111111111111117890 $1000")).to be false
-        expect(subject.send(:valid_instruction?, "Lisa $7")).to be false
-        expect(subject.send(:valid_instruction?, "Charge $7")).to be false
-        expect(subject.send(:valid_instruction?, "Charge Lisa $")).to be false
-        expect(subject.send(:valid_instruction?, "Charge Lisa 7")).to be false
-        expect(subject.send(:valid_instruction?, "Credit Quincy $")).to be false
-        expect(subject.send(:valid_instruction?, "Credit Quincy 200")).to be false
+        expect(processor.send(:valid_instruction?, "")).to be false
+        expect(processor.send(:valid_instruction?, "Add Tom $1000")).to be false
+        expect(processor.send(:valid_instruction?, "Add Tom 41111111111111111")).to be false
+        expect(processor.send(:valid_instruction?, "Add Tom 411111111111111117890 $1000")).to be false
+        expect(processor.send(:valid_instruction?, "Lisa $7")).to be false
+        expect(processor.send(:valid_instruction?, "Charge $7")).to be false
+        expect(processor.send(:valid_instruction?, "Charge Lisa $")).to be false
+        expect(processor.send(:valid_instruction?, "Charge Lisa 7")).to be false
+        expect(processor.send(:valid_instruction?, "Credit Quincy $")).to be false
+        expect(processor.send(:valid_instruction?, "Credit Quincy 200")).to be false
       end
     end
   end
@@ -107,26 +92,164 @@ RSpec.describe Processor do
   describe "#instruction_type" do
     context "when a valid instruction is passed" do
       it "returns the instruction type" do
-        subject = described_class.new
-
-        expect(subject.send(:instruction_type, "Add Tom 4111111111111111 $1000")).to eq :add
-        expect(subject.send(:instruction_type, "Charge Lisa $7")).to eq :charge
-        expect(subject.send(:instruction_type, "Credit Quincy $200")).to eq :credit
+        expect(processor.send(:instruction_type, "Add Tom 4111111111111111 $1000")).to eq :add
+        expect(processor.send(:instruction_type, "Charge Lisa $7")).to eq :charge
+        expect(processor.send(:instruction_type, "Credit Quincy $200")).to eq :credit
       end
     end
 
     context "when an invalid instruction is passed" do
       it "returns nil" do
-        subject = described_class.new
-
-        expect(subject.send(:instruction_type, "")).to be_nil
-        expect(subject.send(:instruction_type, nil)).to be_nil
-        expect(subject.send(:instruction_type, "Credit $200")).to be_nil
-        expect(subject.send(:instruction_type, "Charge Lisa 7")).to be_nil
-        expect(subject.send(:instruction_type, "Lend Quincy $200")).to be_nil
-        expect(subject.send(:instruction_type, "Add Tom 411118382832311111111111 $1000")).to be_nil
+        expect(processor.send(:instruction_type, "")).to be_nil
+        expect(processor.send(:instruction_type, "Add Tom 411111111111111117890 $1000")).to be_nil
+        expect(processor.send(:instruction_type, "Charge Lisa $")).to be_nil
+        expect(processor.send(:instruction_type, "Credit Quincy 200")).to be_nil
       end
     end
   end
 
+  describe "#parse_instruction" do
+    context "when a valid instruction is passed" do
+      it "returns the command parsed as hash" do
+        instruction_hash = processor.parse_instruction(instruction: "Charge Lisa $7")
+        expect(instruction_hash).to eq({ instruction: :charge, user_name: "Lisa", charge_amount: "$7" })
+
+        instruction_hash = processor.parse_instruction(instruction: "Credit Quincy $200")
+        expect(instruction_hash).to eq({ instruction: :credit, user_name: "Quincy", credit_amount: "$200" })
+
+        instruction_hash = processor.parse_instruction(instruction: "Add Tom 4111111111111111 $1000")
+        expect(instruction_hash).to eq(
+          { instruction: :add, user_name: "Tom", card_number: "4111111111111111", card_limit: "$1000" })
+      end
+    end
+
+    context "when an invalid instruction is passed" do
+      it "returns nil" do
+        expect(processor.parse_instruction(instruction: "")).to be_nil
+        expect(processor.parse_instruction(instruction: "Add Tom 411111111111111117890 $1000")).to be_nil
+        expect(processor.parse_instruction(instruction: "Charge Lisa $")).to be_nil
+        expect(processor.parse_instruction(instruction: "Credit Quincy 200")).to be_nil
+      end
+    end
+  end
+
+  describe "#parse_amount" do
+    context "when $<Integer> is passed" do
+      it "returns intenger amount" do
+        expect(processor.send(:parse_amount, "$4300")).to eq 4300
+      end
+    end
+
+    context "when invalid $<Integer> is passed" do
+      it "returns 0" do
+        expect(processor.send(:parse_amount, "$$4300")).to eq 0
+        expect(processor.send(:parse_amount, "$43a00")).to eq 0
+        expect(processor.send(:parse_amount, "test")).to eq 0
+        expect(processor.send(:parse_amount, "")).to eq 0
+      end
+    end
+  end
+
+  describe "#process_instruction_hash" do
+    context "when an 'add' instruction is passed" do
+      it "calls #process_add and returns its result" do
+        instruction_hash = processor.parse_instruction(instruction: "Add Tom 4111111111111111 $1000")
+        new_user = processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.name).to eq "Tom"
+        expect(new_user.card).to have_attributes(number: "4111111111111111", limit: 1000, balance: 0, valid: true)
+
+        instruction_hash = processor.parse_instruction(instruction: "Add Quincy 1234567890123456 $2000")
+        new_user = processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.name).to eq "Quincy"
+        expect(new_user.card).to have_attributes(number: "1234567890123456", limit: 2000, balance: 0, valid: false)
+
+        expect(processor.user_list.length).to eq 2
+      end
+    end
+
+    context "when a 'charge' instruction is passed" do
+      it "calls #process_charge and returns its result" do
+        instruction_hash = processor.parse_instruction(instruction: "Add Tom 4111111111111111 $1000")
+        new_user = processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        instruction_hash = processor.parse_instruction(instruction: "Charge Tom $500")
+        processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.card).to have_attributes(number: "4111111111111111", limit: 1000, balance: 500, valid: true)
+
+        instruction_hash = processor.parse_instruction(instruction: "Charge Tom $500")
+        processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.card).to have_attributes(number: "4111111111111111", limit: 1000, balance: 1000, valid: true)
+
+        instruction_hash = processor.parse_instruction(instruction: "Charge Tom $500")
+        processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.card).to have_attributes(number: "4111111111111111", limit: 1000, balance: 1000, valid: true)
+
+        instruction_hash = processor.parse_instruction(instruction: "Add Quincy 1234567890123456 $2000")
+        new_user = processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.name).to eq "Quincy"
+        expect(new_user.card).to have_attributes(number: "1234567890123456", limit: 2000, balance: 0, valid: false)
+
+        instruction_hash = processor.parse_instruction(instruction: "Charge Quincy $500")
+        processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.card).to have_attributes(number: "1234567890123456", limit: 2000, balance: 0, valid: false)
+        expect(new_user.balance).to eq "error"
+      end
+    end
+
+    context "when a 'credit' instruction is passed" do
+      it "calls #process_credit and returns its result" do
+        instruction_hash = processor.parse_instruction(instruction: "Add Lisa 5454545454545454 $3000")
+        new_user = processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        instruction_hash = processor.parse_instruction(instruction: "Charge Lisa $7")
+        processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.card).to have_attributes(number: "5454545454545454", limit: 3000, balance: 7, valid: true)
+
+        instruction_hash = processor.parse_instruction(instruction: "Credit Lisa $100")
+        processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.card).to have_attributes(number: "5454545454545454", limit: 3000, balance: -93, valid: true)
+
+        instruction_hash = processor.parse_instruction(instruction: "Add Quincy 1234567890123456 $2000")
+        new_user = processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.name).to eq "Quincy"
+        expect(new_user.card).to have_attributes(number: "1234567890123456", limit: 2000, balance: 0, valid: false)
+
+        instruction_hash = processor.parse_instruction(instruction: "Credit Quincy $200")
+        processor.process_instruction_hash(instruction_hash: instruction_hash)
+
+        expect(new_user.card).to have_attributes(number: "1234567890123456", limit: 2000, balance: 0, valid: false)
+        expect(new_user.balance).to eq "error"
+      end
+    end
+  end
+
+  describe "#process_instruction" do
+    context "when a valid instruction is passed, it is processed and its result returned" do
+      it "returns the instruction result (a created User or its balance)" do
+        expect(processor.process_instruction(instruction: "ADD Lisa 5454545454545454 $3000")).to be_a(User)
+        expect(processor.process_instruction(instruction: "ChArGe Lisa $7")).to be 7
+        expect(processor.process_instruction(instruction: "credit Lisa $100")).to be -93
+      end
+    end
+
+
+    context "when a malformed instruction is passed" do
+      it "returns nil" do
+        expect(processor.process_instruction(instruction: "Create Tom 4111111111111111 $1000")).to be_nil
+        expect(processor.process_instruction(instruction: "Add Lisa 1000")).to be_nil
+        expect(processor.process_instruction(instruction: "Charge Quincy $")).to be_nil
+        expect(processor.process_instruction(instruction: "")).to be_nil
+      end
+    end
+  end
 end
